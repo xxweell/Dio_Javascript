4d0c7de3afd0c867410a432133e621e78b0bc292
Wellington Nogueira <wellingtoncw7@gmail.com>
2022-11-22 19:48:43 -0300

    maior valor

--- 07 - importacoes/main.js
+++ 07 - importacoes/main.js
@@ -4,4 +4,31 @@
 
 const {gets, print} = require('./funcoes-auxiliares') // desestruturando o objeto e pegando os métodos gets e print
 
-print(gets())
+/*
+    Uma sala contém 05 alunos e para cada aluno foi sorteado um número de 1 - 100.
+    Faça um programa que receba os 5 números sorteados para os alunos e mostre o maior número sorteado.
+
+    Dados de entrada:
+    5
+    50
+    10
+    98
+    23
+
+    Saída:
+    98
+*/
+
+const quantidadeDeAlunos = gets()
+let maiorValorEncontrado = 0
+
+for (let i = 0; i < quantidadeDeAlunos; i++) { // laço para popular o array 
+    const numeroSorteado = gets()
+    if(numeroSorteado > maiorValorEncontrado) {
+        maiorValorEncontrado = numeroSorteado
+    }
+    
+}
+
+
+print(maiorValorEncontrado)
